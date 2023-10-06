7c6278f4456056090e5fe4daf1b470373f707d7e
Ulises Heredia <uhdesignpro@gmail.com>
2023-10-05 23:25:51 -0500

    finished foundations part1

---
+++ 01_part1_Javascript/first.js
@@ -0,0 +1,48 @@
+
+
+console.log("Hello, World!")
+
+let x,y,result;
+
+x= "2";
+y=  "3";
+
+result= +x + +y;
+
+
+
+console.log("the result is:",result);
+
+// Number(variable) converts variable into a Number
+// +variable also converts variable into a number
+
+let n=2;
+
+n +=4;
+n *=2;
+
+console.log("n=",n);
+
+// starting assignment
+//1
+//Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+
+console.log(23+97);
+//2 Add a sequence of 6 different numbers together.
+let sequence;
+sequence= 3 + 659 +5+4 +94 +4;
+console.log(sequence);
+
+//3 Print the solution to the following equation: (4 + 6 + 9) / 77
+let sequence2;
+sequence2= (4+6 + 9) / 77;
+console.log("the result for problem 3",sequence2);
+
+let a= 10;
+
+let max = 57;
+let actual = max-13;
+let percentage= actual/max;
+console.log("the result for percentage",percentage);
+
+let c=20;
