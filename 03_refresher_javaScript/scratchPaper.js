06718de88da7dba312ce33abcc830c054bd5eba7
Ulises Heredia <uhdesignpro@gmail.com>
2024-03-24 02:08:09 -0500

    created a program that spits out Console log output

---
+++ 03_refresher_javaScript/scratchPaper.js
@@ -0,0 +1,39 @@
+// alert('hello! The Queen')
+
+
+/*  OVERVIEW
+- 8 data types in JavaScript
+- understand  the difference between single , double, and backtick quotes
+- Embed a variable /expression in a string.
+- Understand what a method is
+- Name the three logical operators.
+- Understand what conditionals are .
+- Understanwhat nesting is.
+- Understand what truthy and flasy values are.
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+single quotes '
+
+double quotes  ""
+    
+backtick       `  :embedded javascript into this
+
+*/
+
+
+
+
+// intro to javascript refresher. this code prints out string
+
+
+
+const string1="I ask my self everyday,";
+const string2="am i stupid??";
+const string3=`${string1} ${string2}`;
+
+document.getElementById("demo1").innerHTML= string1;
+document.getElementById("demo2").innerHTML= string2;
+document.getElementById("demo3").innerHTML= string3;
+
+
